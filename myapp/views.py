f50036800a5986da068a54964c5581a5bea54621
shivamj2200 <shivamjaiswal2200@gmail.com>
2021-06-26 21:12:47 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import os
+import pickle
+
+# Create your views here.
+def index(request):
+     return render(request,'index.html')
+def test(request):
+    ppw=int(request.POST['ppw'])
+    pn = int(request.POST['pn'])
+    mi=int(request.POST['mi'])
+    appm=int(request.POST['appm'])
+    path=os.path.dirname(__file__)# this will give you root path
+    model=pickle.load(open(os.path.join(path,'taxi.pkl'),'rb'))
+    res=str(model.predict([[ppw,pn,mi,appm]])[0].round(2))
+    return render(request,"index.html",{'res':res})
